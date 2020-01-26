5714d5ede752c81c327b523e73a5dacebd8683c7
Joey Huab <joey@evolution-x.org>
2020-01-26 05:41:07 +0000

    raphael: [SQUASH] Revert some imports
    Revert "raphael: import perfboost config from hotdog"
    
    This reverts commit 550da79f7a8baf0a4d887c2dbbc919f290010496.
    
    Revert "raphael: import msm irqbalance from flame"
    
    This reverts commit e6d09cfaed824b1c57c5eb9c52ddfa9d1a782428.
    
    Revert "raphael: import powerhint from hotdog"
    
    This reverts commit a3f3b479df11c57a319478c209f462711fed7d63.

--- device.mk
+++ device.mk
@@ -133,17 +133,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/libnfc-nci.conf
 
-# Perf
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/msm_irqbalance.conf \
-    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/perf/perfboostsconfig.xml
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service.raphael
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/powerhint.xml
 
 # Ril
 PRODUCT_PACKAGES += \
